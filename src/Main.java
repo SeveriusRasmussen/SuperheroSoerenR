f2f04cbe5ec3b544963193b9cc534c3f5efbbd32
SeveriusRasmussen <sora0001@stud.kea.dk>
2023-09-11 15:56:50 +0200

    Søren R. 11 sept.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner (System.in);
+        System.out.println("Your superhero name is: ");
+        String name = keyboard.next();
+        System.out.println("Your real name is: ");
+        String realname = keyboard.next();
+        System.out.println("Your superpower is: ");
+        String superPower = keyboard.next();
+        System.out.println("You was created in: ");
+        String yearCreated = keyboard.next();
+        System.out.println("Are you a human?");
+        Boolean isHuman = keyboard.hasNextBoolean();
+        System.out.println("How strong are you?");
+        double strength = keyboard.nextDouble();
+
+    }
 
 
 
--- src/Superhero.java
+++ src/Superhero.java
@@ -1,6 +1,53 @@
 public class Superhero {
     private String name;
-    private
+    private String realname;
+    private String superPower;
+    private int yearCreated;
+    private boolean isHuman;
+    private String strength;
 
+    public Superhero(String name, String realName, String superPower,
+                     int yearCreated, boolean isHuman, String strength) {
+        this.name = name;
+        this.realname = realName;
+        this.superPower = superPower;
+        this.yearCreated = yearCreated;
+        this.isHuman = isHuman;
+        this.strength = strength;
+    }
+
+
+    public String name(){
+        return name;
+    }
+
+    public String realname(){
+        return realname;
+    }
+
+    public String superPower(){
+        return superPower;
+    }
+
+    public int yearCreated(){
+        return yearCreated;
+    }
+
+    public boolean isHuman(){
+        return isHuman;
+    }
+
+    public String strength(){
+        return strength;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    /* public String toString() {
+
+    } */
 
 }
